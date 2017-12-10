464b181801d29d9039aca19b8d3b82bda2d16691
SalaniUNITN <francesco.salani@studenti.unitn.it>
2017-12-10 19:45:52 +0100

    added other tests

---
+++ AppFinale/features/step_definitions/show_daily_bill.steps.rb
@@ -0,0 +1,3 @@
+When("I dont type in some information (e.g. name)") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ AppFinale/features/step_definitions/show_invoices_steps.rb
@@ -0,0 +1,55 @@
+Then("I should see my clients information and a list of the invoices related to him") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I type {string} in the search bar") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("If the list of invoices is empty I should see my clients information and a message {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("there are clients related to me in the DB") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I dont type in some information (e.g. name)") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the main page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the list of the invoices inserted by me during the day grouped into {string} and {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see the total amount") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("There are no invoices inserted by me during the day") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("there are invoices in the DB") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see a list the invoices created by me") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am viewing the list of invoices") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The invoice should be deleted from the DB") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("The invoices field should be set to {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
